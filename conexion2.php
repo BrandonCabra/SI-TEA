37dac3fd2e174589970a5a76e856f038896a962d
Deiner08 <deinergarcesht@gmail.com>
2025-04-28 20:11:07 -0500

    cambios en la estructura de carpetas

--- conexion2.php
+++ conexion2.php
@@ -3,7 +3,7 @@ $host = "localhost";
 $username = "root"; // Cambia este valor si tu usuario es diferente
 $password = ""; // Cambia este valor si tienes una contraseña
 $dbname = "sitea4"; // Cambia este valor si tu base de datos tiene otro nombre
-$port=3306;
+$port = 3306;
 
 if ($conexion = new mysqli($host, $username, $password, $dbname, $port)) {
     echo "Conexión exitosa a la base de datos";
